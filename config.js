7165485bc0cdc1cd2328646e8d62d010401fa183
kronos1128 <kronos110901@gmail.com>
2026-04-27 19:56:58 +0200

    Premier envoi - Stockage B&W

---
+++ config.js
@@ -0,0 +1,7 @@
+window.CONFIG = {
+    // API principale pour Black And White
+    API_URL: "https://script.google.com/macros/s/AKfycbx5647gaky0XsxBWwSXJsnTprXu8rMKd_phCtpQID6ogowzYtvRl3zCsoO0r1OuccJ7/exec",
+    
+    // Webhook Discord spécifique à Black And White
+    DISCORD_WEBHOOK: "https://discord.com/api/webhooks/1498327195276415066/JPaYFYK46Ik6RKFMv6eQ9jnlvR9mZOUJRyrtWxGBXRFk0zO5KzYuJJtC8pYRQYoy0hWM"
+};
