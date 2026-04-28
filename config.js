3695548150e67850e3f7023763a0a58830fe7524
onyxbloom09-stack <onyxbloom09@gmail.com>
2026-04-29 01:22:07 +0200

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
     // API principale pour Black And White
-    API_URL: "https://script.google.com/macros/s/AKfycbyrBVQ4EuBAlaV0NLUtAlHxz1_2Qp3T4jzkD3d5cBRPSf9e1YGBHTXpMEMuKbpzkwc/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbwiGT1_JlK68hIQCceNLei_is1x7nb5qH7HFmoTvb-YmHcwEErJ-k4wGQW9KOUB3UVB/exec",
     
     // Webhook Discord spécifique à Black And White
     DISCORD_WEBHOOK: "https://discord.com/api/webhooks/1498327195276415066/JPaYFYK46Ik6RKFMv6eQ9jnlvR9mZOUJRyrtWxGBXRFk0zO5KzYuJJtC8pYRQYoy0hWM"
